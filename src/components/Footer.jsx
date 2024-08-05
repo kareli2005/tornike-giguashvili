10f77c38059dca7286d58a636bb342544b0086cb
kareli2005 <cotneqareli123@gmail.com>
2024-08-05 23:44:48 +0400

    Your commit message

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
     <div className='w-full bg-gray-950 flex flex-col justify-center items-center border-slate-400 border-t text-slate-400 duration-300'>
       <div className='w-full max-w-[1280px] flex flex-col md:flex-row justify-between items-start gap-8 md:gap-16 p-4 md:py-8 md:px-8 xl:px-0'>
         <div className='flex flex-col items-start justify-start w-full md:w-1/2 lg:w-1/3'>
-          <h1 className='text-2xl text-white uppercase'>
+          <h1 className='text-2xl text-white text-center md:text-start w-full uppercase'>
             About Me
           </h1>
           <div className='w-full flex justify-center py-4'>
@@ -14,7 +14,7 @@ const Footer = () => {
           <p className='text-justify'>I’m Tornike Giguashvili, a passionate photographer with a love for capturing life's most memorable moments. With over 5 years of experience in photography, I specialize in portrait and event photography. I’m dedicated to creating stunning visuals that tell a story, and I'm always eager to explore new creative challenges. When I’m not behind the lens, you can find me exploring the outdoors or experimenting with new photography techniques.          </p>
         </div>
         <div className='flex flex-col items-start justify-start w-full md:w-1/2 lg:w-1/3'>
-          <h1 className='text-2xl text-white uppercase'>
+          <h1 className='text-2xl text-white text-center md:text-start w-full uppercase'>
             Contact Information
           </h1>
           <div className='w-full flex justify-center py-4'>
@@ -30,11 +30,14 @@ const Footer = () => {
         <div className='h-[2px] w-full max-w-[1280px] bg-slate-400' />
       </div>
       <div className='w-full max-w-[1280px] flex items-center py-4 uppercase flex-col md:flex-row justify-between gap-4 text-sm md:text-base px-4 md:px-8 xl:px-0'>
-        <p>
+        <p className='text-center md:text-start'>
           ©2024 Tornike Giguashvili. All Rights Reserved.
         </p>
-        <p className='text-white md:text-slate-400'>
-          site by <a rel='noreferrer' target='_blank' href='https://github.com/kareli2005/tornike-giguashvili' className='cursor-pointer hover:text-white hover:underline duration-300'>Tsotne Kareli</a>
+        <div className='w-full flex justify-center py-4 md:hidden'>
+          <div className='h-[2px] w-full max-w-[1280px] bg-slate-400' />
+        </div>
+        <p className='text-slate-400 text-center md:text-end'>
+          website by <a rel='noreferrer' target='_blank' href='https://github.com/kareli2005' className='cursor-pointer hover:text-white hover:underline duration-300'>Tsotne Kareli</a>
         </p>
       </div>
     </div>
--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -20,7 +20,7 @@ const Gallery = () => {
       imageRefs.current.forEach((ref, index) => {
         if (ref) {
           const rect = ref.getBoundingClientRect()
-          const isVisible = rect.top < window.innerHeight && rect.bottom >= 0
+          const isVisible = rect.top < window.innerHeight
           if (isVisible) {
             ref.classList.add('enter-animation')
           }
@@ -62,8 +62,8 @@ const Gallery = () => {
   }
 
   return (
-    <div id='gallery' className='w-full h-auto bg-gray-950 text-slate-400 flex flex-col items-center justify-start py-8'>
-      <div className='flex flex-col justify-center items-center gap-4 p-8 max-w-[1024px]'>
+    <div id='gallery' className='w-full h-auto bg-gray-950 text-slate-400 flex flex-col items-center justify-start pt-8'>
+      <div className='flex flex-col justify-center items-center gap-4 p-4 md:p-8 max-w-[1024px]'>
         <h1 className='text-2xl leading-[27px] sm:text-4xl sm:leading-[35px] md:text-[54px] md:leading-[56px] text-slate-200 text-center uppercase'>
           Welcome To Gallery
         </h1>
@@ -76,7 +76,7 @@ const Gallery = () => {
         <div className='h-[2px] w-1/2 md:w-1/3 bg-slate-400' />
       </div>
 
-      <div className='w-full max-w-[1280px] py-8 px-4 flex flex-col gap-4 md:gap-8 justify-center items-center md:px-8 xl:px-0 duration-300'>
+      <div className='w-full max-w-[1280px] py-4 md:py-8 px-4 flex flex-col gap-4 md:gap-8 justify-center items-center md:px-8 xl:px-0 duration-300'>
         <div className='text-xs md:text-sm lg:text-base w-full flex flex-wrap justify-start items-center gap-2 md:gap-4'>
           <button
             onClick={() => setCategory('all')} 
@@ -114,11 +114,11 @@ const Gallery = () => {
                 <div className='bg-gray-950 bg-opacity-70 image-hover-div'>
                   <button
                     onClick={(e) => handleImageClick(item.path, e)} 
-                    className='duration-300 hover:border-white hover:text-white border-slate-400 cursor-pointer uppercase px-4 py-2 border rounded-md'>
+                    className='duration-300 hover:border-white hover:text-white border-slate-400 uppercase px-4 py-2 border rounded-md'>
                       view original size
                   </button>
                 </div>
-                <img className='object-cover w-full h-full rounded-md duration-300 cursor-pointer' src={item.path} alt={item.name} />
+                <img className='object-cover w-full h-full rounded-md duration-300' src={item.path} alt={item.name} />
               </div>
             ))
           )}
@@ -126,11 +126,11 @@ const Gallery = () => {
 
         {
           numOfImages <= filteredImages.length ? 
-            <button onClick={() => setNumOfImages(prev => prev + 20)} className='hover:text-white duration-300 my-8 text:sm md:text-lg'>
+            <button onClick={() => setNumOfImages(prev => prev + 20)} className='hover:text-white duration-300 my-4 md:my-8 text:sm md:text-lg'>
               see more...
             </button>
             : numOfImages > 20 &&
-              <button onClick={() => setNumOfImages(20)} className='hover:text-white duration-300 my-8 text:sm md:text-lg'>
+              <button onClick={() => setNumOfImages(20)} className='hover:text-white duration-300 my-4 md:my-8 text:sm md:text-lg'>
                 show less
               </button>
         }
